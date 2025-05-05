1305f47d6bb3c353e0b4b6a36b24a5b77833aff5
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-05 20:57:16 +0600

    access modifier

---
+++ obj-oriented-typescript/src/access-modifires.ts
@@ -0,0 +1,46 @@
+{
+
+    class BankAccount{
+        public readonly id: number;
+        public name:string;
+        // private _balance: number;
+        protected _balance: number;        // use protected for access in child class
+
+
+        constructor(id: number, name: string, _balance: number){
+            this.id = id;
+            this.name = name;
+            this._balance = _balance;
+        }
+
+        addDeposit(amount: number){
+            this._balance = this._balance + amount
+            // console.log(this.balance);
+        }
+
+        getBalance(){
+            return this._balance;
+        }
+    }
+
+    class Student extends BankAccount{
+        text(){
+            this._balance
+        }
+    }
+
+    const person = new BankAccount(10,'nazmul', 1000)
+    // person.balance
+    // person.balance = 2000;
+    // console.log(person.balance);
+    // person.id =
+    person.addDeposit(5000)
+    const newBalance = person.getBalance()
+    console.log(newBalance);
+
+
+
+
+
+
+}
