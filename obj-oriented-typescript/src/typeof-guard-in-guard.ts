7c2b48d951383b4b7634571d59ea14e6eb82eced
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-05-05 19:50:47 +0600

    in guard

--- obj-oriented-typescript/src/typeof-guard-in-guard.ts
+++ obj-oriented-typescript/src/typeof-guard-in-guard.ts
@@ -6,7 +6,7 @@
     //typeof
 
     type Alphanewmeric = number | string;
-    
+
 
 
     // const add = (param1: number | string, param2: number | string): number | string => {
@@ -23,6 +23,42 @@
     const Add2 = add(2, 3)
     const Add3 = add("2", "3")
     console.log(Add, Add2, Add3);
+
+
+    // in guard
+
+    type NormalUser = {
+        name: string;
+    }
+
+
+    type AdminUser = {
+        name: string;
+        role: 'admin'
+    }
+
+    const getUser = (user: NormalUser | AdminUser) => {
+
+        if ('role' in user) {
+            user.role
+            console.log(`My name is ${user.name} and my role is ${user.role}`);
+        }
+        else {
+            console.log(`My name is ${user.name}`);
+        }
+    }
+
+    const normalUser: NormalUser = {
+        name: 'mr normal'
+    }
+    const adminUser: AdminUser = {
+        name: 'mr admin',
+        role: 'admin'
+    }
+
+    getUser(normalUser)
+    getUser(adminUser)
+    // const user = normalUser.name
 
 
 }
